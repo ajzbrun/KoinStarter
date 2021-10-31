8b6bf70b0de895f8259175981ed229545942982a
azbrun <ajzbrun@gmail.com>
2021-10-31 20:12:54 -0300

    little improvements in campaign creation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Button, Form, Input } from 'semantic-ui-react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
@@ -7,24 +7,34 @@ import web3 from '../../ethereum/web3';
 import Layout from '../../components/Layout';
 
 const newCampaign = () => {
+    const[errMessage, setErrorMessage] = useState('');
     const[minimumContribution, setMinimumContribution] = useState('');
+    const[isLoading, setIsLoading] = useState(false);
 
     const onSubmit = async (event) => {
         event.preventDefault();
+        setErrorMessage('');
+        setIsLoading(true);
+        
+        try{
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            setErrorMessage(err.message);
+        }
 
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(minimumContribution)
-            .send({
-                from: accounts[0]
-            });
+        setIsLoading(false);
     }
 
     return (
         <Layout>
             <h3>Create a Campaign</h3>
             
-            <Form onSubmit={onSubmit}>
+            <Form onSubmit={onSubmit} error={!!errMessage}>
                 <Form.Field>
                     <label>Minimum contribution</label>
                     <Input 
@@ -37,7 +47,9 @@ const newCampaign = () => {
                 <Form.Field>
                     <label>By submitting I agree to the Terms and Conditions of KoinStarter</label>
                 </Form.Field>
-                <Button type='submit' primary>Create</Button>
+
+                <Message error header="Attention" content={errMessage} /> 
+                <Button type='submit' loading={isLoading} primary>Create</Button>
             </Form>
         </Layout>
     )
